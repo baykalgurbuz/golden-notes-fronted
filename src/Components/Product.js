284de1a95ff3ac63c0cddb052a086cafac336833
baykalgurbuz <ebaykalgurbuz@gmail.com>
2024-06-19 13:08:38 +0300

    Products names fetched db.

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,32 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Product = () => {
   const [product, setProduct] = useState('');
   const [quantity, setQuantity] = useState('');
-  const [unit, setUnit] = useState('kg'); // varsayılan olarak kg seçili
-  const [note, setNote] = useState('');
+  const [unit, setUnit] = useState('kg');  
+  const [note, setNote] = useState(''); 
+  const [salesList, setSalesList] = useState([]);
+  const [products, setProducts] = useState([]);
+
+ 
+
+  useEffect(() => {
+    fetchSales();
+    fetchProducts();
+  }, []);
+
+  const fetchProducts=async()=>{
+    try {
+      const response =await fetch('http://localhost:8080/api/product');
+     if (!response.ok) {
+      throw new Error("Fetchleyemedim.")
+     } 
+     const data =await response.json();
+     console.log(response)
+     setProducts(data);
+    } catch (error) {
+      console.log("Fetch error",error)
+    }
+  }
+
+  const fetchSales = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/sale');
+      if (!response.ok) {
+        throw new Error("Fetchleyemedim.")
+       } 
+       const data = await response.json();
+    setSalesList(data);
+    } catch (error) {
+      console.log("Fetch error",error)
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = { product, quantity, unit, note };
-
-    const response = await fetch('http://localhost:8080/api/product', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      alert('Product saved successfully');
-      setProduct('');
-      setQuantity('');
-      setUnit('kg');
-      setNote('');
-    } else {
-      alert('Failed to save product');
+    const formData = { product, quantity, unit, note   };
+    try {
+      const response = await fetch('http://localhost:8080/api/sale', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        alert('Sales saved successfully');
+        setProduct('');
+        setQuantity('');
+        setUnit('kg');
+        setNote('');
+        fetchSales();
+      } else {
+        alert('Failed to save product');
+      }
+    } catch (error) {
+      console.error('Error save sale:', error);
+      alert('Failed to save sale');
     }
+
   };
 
   return (
@@ -34,14 +76,20 @@ const Product = () => {
          <form onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="productName">Product Name:</label>
-        <input
-          type="text"
-          className="form-control"
-          id="productName"
-          value={product}
-          onChange={(e) => setProduct(e.target.value)}
-          required
-        />
+        <select
+            id="productName"
+            className="form-control"
+            value={product}
+            onChange={(e) => setProduct(e.target.value)}
+            required
+          >
+            <option value="">Select a product</option>
+            {products.map((prod) => (
+              <option key={prod.id} value={prod.productName}>
+                {prod.productName}
+              </option>
+            ))}
+          </select>
       </div>
       <div className="form-row">
         <div className="form-group col-md-6">
@@ -78,9 +126,38 @@ const Product = () => {
         />
       </div>
       <button type="submit" className="btn btn-primary">
-        Save Product
+        Save Sale
       </button>
     </form>
+    <div className="mt-5">
+        <h2>Sale List</h2>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Product</th>
+              <th>Quantity</th>
+              <th>Unit</th>
+              <th>Note</th>
+          
+            </tr>
+          </thead>
+          <tbody>
+            {salesList.map((sale) => (
+              <tr key={sale.id}>
+                <td>{sale.id}</td>
+                <td>{sale.product}</td>
+                <td>{sale.quantity}</td>
+                <td>{sale.unit}</td>
+                <td>{sale.note}</td>
+              </tr>
+            ))}
+            {
+              console.log(salesList)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
    
   );
